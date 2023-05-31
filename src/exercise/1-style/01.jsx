be88115ebe6281ebc1e1af01c572683e8f5536a1
Julien Goletto <julien.goletto@gmail.com>
2023-05-31 11:35:42 +0200

    :building_construction: CSS in JS is in da place

--- src/exercise/1-style/01.jsx
+++ src/exercise/1-style/01.jsx
@@ -1,5 +1,6 @@
-// import styles from "./01.module.css";
-import clsx from "clsx";
+import styles from "./01.module.css";
+// import clsx from "clsx";
+import styled from 'styled-components';
 
 // const VariantsStyle = { 
 //   primary: { backgroundColor: '#646cff'},
@@ -12,7 +13,20 @@ import clsx from "clsx";
 //   large: { padding: '12px 24px', fontSize: '1.2rem'},
 // }
 
-const Button = ({ variant = 'default', size, children }) => {
+const Button = styled.button({
+  backgroundColor: ({variant}) => {
+    if (variant === 'primary') return '#646cff';
+    if (variant === 'secondary') return '#16a34a';
+    return '#171717';
+  },
+  color: 'white',
+  borderRadius: 4,
+  borderColor: 'transparent',
+  padding: ({size}) => size === 'small' ? '8px 16px' : '12px 24px',
+  fontSize: ({size}) => size === 'large' ? '1rem' : '1.2rem',
+});
+
+  // const Button = ({ variant = 'default', size, children }) => {
   // const variantStyle = VariantsStyle[variant];
   // const sizeStyle = SizeStyles[size];
   // const style = { 
@@ -21,35 +35,35 @@ const Button = ({ variant = 'default', size, children }) => {
   //   ['--font-size']: sizeStyle.fontSize,
   // }
 
-  return (
-  <button
-    // className={styles.button}
-    className={
-      clsx(
-        "rounded text-white font-medium pointer border border-2 border-transparent hover:border-blue-500 focus:outline-none focus:border-red-800",
-        { 
-          'bg-[#646cff]': variant === "primary",
-          'bg-green-600': variant === "secondary",
-          'bg-neutral-900': variant === "default",
-        },
-        {
-          'px-2 py-4 text-base': size === 'small',
-          'px-3 py-6 text-lg': size === 'large',
-        }
-      )
-    }
-    // style={style}
-    >
-    {children}
-  </button>);
-};
+//   return (
+//   <button
+//     // className={styles.button}
+//     className={
+//       clsx(
+//         "rounded text-white font-medium pointer border border-2 border-transparent hover:border-blue-500 focus:outline-none focus:border-red-800",
+//         { 
+//           'bg-[#646cff]': variant === "primary",
+//           'bg-green-600': variant === "secondary",
+//           'bg-neutral-900': variant === "default",
+//         },
+//         {
+//           'px-2 py-4 text-base': size === 'small',
+//           'px-3 py-6 text-lg': size === 'large',
+//         }
+//       )
+//     }
+//     // style={style}
+//     >
+//     {children}
+//   </button>);
+// };
 
 const Demo = () => {
   return (
     <div 
-      // style = {{['--bg-color']: '#F1F1F1'}}
-      // className={styles.container}
-      className="flex flex-col items-center gap-2"
+      style = {{['--bg-color']: '#F1F1F1'}}
+      className={styles.container}
+      // className="flex flex-col items-center gap-2"
     >
       <Button variant="primary" size="small">
         Primary small
